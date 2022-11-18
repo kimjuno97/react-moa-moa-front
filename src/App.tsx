e2049e98cab30b29a65ae39f9200bf830382867c
kimjuno97 <jh241093@gmail.com>
2022-11-18 15:35:51 +0900

    fix: index.tsx, App,tsx import 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@
 import Page from './모달/Page';
 import InfiniteScroll from './무한스크롤/InfiniteScroll';
 import MoveBox from './박스드래그/MoveBox';
+import ImgUpload from './이미지파일업로드/ImgUpload';
 
 export default function App() {
-	return <MoveBox />;
+	return <Page />;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import ImgUpload from './이미지파일업로드/ImgUpload';
 
-export const root = ReactDOM.createRoot(
+const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
-root.render(<ImgUpload />);
+root.render(<App />);
